9b902d82416731b94bea08d12907d0ec47d0a016
WannyMartins <wcatery@gmail.com>
2021-12-13 16:17:52 -0300

    requisito 8

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.onload = [setColorBlack, setBackgroundColor]
      
     
     function setColorBlack(){
-    let corInicial = document.querySelector('#primeira');
+    let corInicial = document.querySelector('#black');
     corInicial.className ='color selected'
     }
     
@@ -11,24 +11,48 @@ window.onload = [setColorBlack, setBackgroundColor]
     function setBackgroundColor() {
         let cor = document.querySelector(".pixel")
         cor.style.backgroundColor = 'white'
-    }
+        }
 
        let colors = document.querySelectorAll('.color')
        for (let i = 0; i < colors.length; i+=1){
-        colors[i].addEventListener("click", mudaCor)
-    }
-    function mudaCor(item) {
+        colors[i].addEventListener("click", mudaClasseSlected)
+        }
+    function mudaClasseSlected(item) {
         for (let i = 0; i < colors.length; i+=1){
             colors[i].className = "color"
         } 
         item.target.className = "color selected"
     } // vi esse código do Rodrigo castro no slack
 
-      
-    
 
+    document.querySelector('#black').style.backgroundColor = 'black'
+document.querySelector('#blue').style.backgroundColor = 'blue'
+document.querySelector('#yellow').style.backgroundColor = 'yellow'
+document.querySelector('#green').style.backgroundColor = 'green'
 
+var pixel = document.querySelectorAll('.pixel');
  
+ 
+for(let i =0; i <pixel.length;i+=1){
+
+    pixel[i].addEventListener('click', function(event){
+        let selected = document.querySelector('.selected').style.backgroundColor;  
+        event.target.style.backgroundColor = selected;   
+    } )
+}
+    ////////////////////////
+/* let apagaCorPixel = document.querySelector('#clear-board');
+var pixelBoard = document.querySelector('#pixel-board')
+
+apagaCorPixel.addEventListener('click', function (event){
+    while(document.querySelector('.pixel').style.backgroundColor!== 'white'){
+        var branco= document.querySelector('.pixel');
+        branco.style.backgroundColor='white'
+    }
+   
+ })
+
+ */ 
 
 
 //Referencias
